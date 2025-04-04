02284f2129fa65c7972d86ba8ab10495ef67c8cb
Sanerita <sanelisiwe.sileku@gmail.com>
2025-04-05 00:25:27 +0200

    product list container and table

---
+++ client/src/pages/admin/ProductListPage.jsx
@@ -0,0 +1,91 @@
+import { useEffect } from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Table, Button, Row, Col } from 'react-bootstrap';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+import { useGetProductsQuery, useDeleteProductMutation } from '../../slices/productsApiSlice';
+import { toast } from 'react-toastify';
+import Message from '../../components/Message';
+import Loader from '../../components/Loader';
+
+const ProductListPage = () => {
+  const { data: products, isLoading, error, refetch } = useGetProductsQuery();
+  const [deleteProduct, { isLoading: loadingDelete }] = useDeleteProductMutation();
+
+  const deleteHandler = async (id) => {
+    if (window.confirm('Are you sure you want to delete this product?')) {
+      try {
+        await deleteProduct(id);
+        refetch();
+        toast.success('Product deleted');
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
+    }
+  };
+
+  return (
+    <>
+      <Row className='align-items-center'>
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className='text-end'>
+          <Button className='btn-sm m-3' onClick={refetch}>
+            Refresh
+          </Button>
+          <LinkContainer to='/admin/product/create'>
+            <Button className='btn-sm'>
+              <FaPlus /> Create Product
+            </Button>
+          </LinkContainer>
+        </Col>
+      </Row>
+
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className='table-sm'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>PRICE</th>
+              <th>CATEGORY</th>
+              <th>BRAND</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product._id}>
+                <td>{product._id}</td>
+                <td>{product.name}</td>
+                <td>${product.price}</td>
+                <td>{product.category}</td>
+                <td>{product.brand}</td>
+                <td>
+                  <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                    <Button variant='light' className='btn-sm'>
+                      <FaEdit />
+                    </Button>
+                  </LinkContainer>
+                  <Button
+                    variant='danger'
+                    className='btn-sm'
+                    onClick={() => deleteHandler(product._id)}
+                  >
+                    <FaTrash />
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  );
+};
+
+export default ProductListPage;
